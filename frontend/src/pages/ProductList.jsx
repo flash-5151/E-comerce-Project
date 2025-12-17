a23dc9726468e96f45e21b0a01f44b908c2b9f2d
flash <bandichakri15@gmail.com>
2025-12-17 18:22:55 +0000

    spinner is fixed

--- frontend/src/pages/ProductList.jsx
+++ frontend/src/pages/ProductList.jsx
@@ -23,11 +23,9 @@ const ProductList = () => {
           //   No Products available
 
           // </div>
-          <div className="flex items-center justify-center py-10">
-            <div
-              className={`h-12 w-12 animate-spin rounded-full border-4 border-gray-300 border-t-blue-600`}
-            />
-          </div>
+          <div
+            className={`h-12 w-12 animate-spin rounded-full border-4 border-gray-300 border-t-blue-600`}
+          />
         )}
       </div>
     </div>
