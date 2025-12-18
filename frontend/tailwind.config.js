c8e0fc480fa338032461d755240a4024cff26e6f
flash <bandichakri15@gmail.com>
2025-12-18 16:44:14 +0000

    fixed

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,16 +1,8 @@
-// /** @type {import('tailwindcss').Config} */
-// export default {
-//   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
-//   theme: {
-//     screens: {
-//       xs: "320px", // Mobile S
-//       sm: "375px", // Mobile M
-//       md: "425px", // Mobile L
-//       lg: "768px", // Tablet
-//       xl: "1024px", // Laptop
-//       "2xl": "1280px",
-//     },
-//     extend: {},
-//   },
-//   plugins: [],
-// };
+export default {
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  darkMode: "class", // 👈 REQUIRED
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
