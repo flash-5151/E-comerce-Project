dd0df40075350d00b19eec7a364ac0c18be897b4
flash <bandichakri15@gmail.com>
2025-12-20 10:14:26 +0000

    styling to header is added

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -19,10 +19,7 @@ const Navbar = () => {
 
   return (
     <nav className="fixed top-0 left-0 w-full h-16 bg-white font-bold shadow-md z-50 flex items-center justify-between  ">
-      <Link
-        to="/"
-        className="cursor-pointer font-bold px-4 sm:px-16 text-2xl hover:border-2 hover:border-gray-400 transition-all duration-300 ease-in-out"
-      >
+      <Link to="/" className="cursor-pointer font-bold px-4 sm:px-16 text-2xl ">
         Home
       </Link>
 
@@ -30,17 +27,14 @@ const Navbar = () => {
         <div className="font-bold px-2 sm:px-16 text-2xl ">
           {!isLoggedIn ? (
             <>
-              <Link
-                to="/login"
-                className="ml-3 mr-0 cursor-pointer hover:border-2 hover:border-gray-400 transition-all duration-300 ease-in-out"
-              >
+              <Link to="/login" className="ml-3 mr-0 cursor-pointer ">
                 Login
               </Link>
             </>
           ) : (
             <>
               <button
-                className="ml-3 mr-0 cursor-pointer hover:border-2 hover:border-gray-400 transition-all duration-300 ease-in-out"
+                className="ml-3 mr-0 cursor-pointer "
                 onClick={handleLogout}
               >
                 Logout
@@ -49,7 +43,7 @@ const Navbar = () => {
           )}
         </div>
         <Link to="/cart" className="p-0 ml-0">
-          <span className="relative inline-block ml-1 cursor-pointer hover:border-2 hover:border-gray-400 transition-all duration-300 ease-in-out">
+          <span className="relative inline-block ml-1 cursor-pointer ">
             🛒
             {cartCount > 0 && (
               <span className="absolute -top-1 -right-1 bg-red-600 text-white text-xs min-w-[18px] h-[18px] px-1 flex items-center justify-center rounded-full">
