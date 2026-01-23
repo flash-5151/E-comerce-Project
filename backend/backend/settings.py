5cbf07ce724dc1268f511c324a1d77ef42aeb7fc
flash <bandichakri15@gmail.com>
2026-01-23 17:58:54 +0000

    kettle pic is uploaded

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -65,19 +65,28 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 
 
 # DATABASE — SAME VALUES, JUST FROM ENV
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': os.getenv("DB_NAME"),
-        'USER': os.getenv("DB_USER"),
-        'PASSWORD': os.getenv("DB_PASSWORD"),
-        'HOST': os.getenv("DB_HOST"),
-        'PORT': os.getenv("DB_PORT"),
-        'OPTIONS': {
-            'sslmode': 'require',
-        },
+if os.getenv("RENDER") == "true":
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.postgresql',
+            'NAME': os.getenv('DB_NAME'),
+            'USER': os.getenv('DB_USER'),
+            'PASSWORD': os.getenv('DB_PASSWORD'),
+            'HOST': os.getenv('DB_HOST'),
+            'PORT': os.getenv('DB_PORT'),
+            'OPTIONS': {
+                'sslmode': 'require',
+            },
+        }
     }
-}
+else:
+    DATABASES = {
+        "default": {
+            "ENGINE": "django.db.backends.sqlite3",
+            "NAME": BASE_DIR / "db.sqlite3",
+        }
+    }
+
 
 
 AUTH_PASSWORD_VALIDATORS = [
